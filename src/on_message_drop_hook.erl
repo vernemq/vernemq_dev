cf70588712f79aa3200737ebf1ffcca4d92e240f
Andre Graf <andre@dergraf.org>
2018-08-17 14:14:18 +0200

    Add on_message_drop callback

---
+++ src/on_message_drop_hook.erl
@@ -0,0 +1,9 @@
+-module(on_message_drop_hook).
+-include("vernemq_dev.hrl").
+
+%% called as an 'all'-hook, return value is ignored
+-callback on_message_drop(SubscriberId  :: subscriber_id(),
+                          Promise       :: fun(() -> {topic(), qos(), payload(), map()} | error),
+                          Reason        :: expired
+                                         | queue_full
+                                         | max_packet_size_exceeded) -> any().

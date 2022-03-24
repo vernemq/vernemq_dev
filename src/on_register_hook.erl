ac25145cae2c4a0a390579181790d2a31d79c0cd
Swagat Parida <swagat.parida@gojek.com>
2022-03-24 17:03:27 +0530

    Add user properties to on_register hook

--- src/on_register_hook.erl
+++ src/on_register_hook.erl
@@ -5,4 +5,5 @@
 %% called as an 'all' hook, return value is ignored
 -callback on_register(Peer          :: peer(),
                       SubscriberId  :: subscriber_id(),
-                      UserName      :: username()) -> any().
+                      UserName      :: username(),
+                      UserProperties    :: properties()) -> any().

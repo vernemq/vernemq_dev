18ff6d264fc73e51669fa02f8be107c6b56adec1
mths1 <mths1>
2024-02-20 13:49:18 +0100

    Add properties to on_deliver_hook

--- src/on_deliver_hook.erl
+++ src/on_deliver_hook.erl
@@ -1,16 +1,30 @@
 %% @hidden
 -module(on_deliver_hook).
--include("vernemq_dev.hrl").
+-include("vernemq_dev_int.hrl").
 
 -callback on_deliver(UserName      :: username(),
                      SubscriberId  :: subscriber_id(),
                      QoS           :: qos(),
                      Topic         :: topic(),
                      Payload       :: payload(),
-                     IsRetain      :: flag()) -> ok
+                     IsRetain      :: flag(),
+                     Properties    :: deliver_properties()) ->
+                                                   ok
                                                  | {ok, Payload    :: payload()}
                                                  | {ok, Modifiers  :: [msg_modifier()]}
                                                  | next.
+
+-type deliver_properties() ::
+        #{
+          p_payload_format_indicator => unspecified | utf8,
+          p_message_expiry_interval => seconds(),
+          p_topic_alias => 1..65535,
+          p_response_topic => topic(),
+          p_correlation_data => binary(),
+          p_user_property => nonempty_list(user_property()),
+          p_subscription_id => [subscription_id()],
+          p_content_type => utf8string()
+         }.
 
 -type msg_modifier() ::
         %% Rewrite the topic of the message.
